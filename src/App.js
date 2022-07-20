7e6b8e89e7488e18f1ca4e745c3e1d9274bd00e7
Hamza <hafizhamza660@gmail.com>
2022-07-21 02:41:06 +0500

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import { data } from "./data/dummy-data";
 
 function App() {
+  const [ids, setIds] = useState(data.option);
+  const [subOptions] = useState(data.subOption);
+  const [newArr, setNewArr] = useState([]);
+
+  const setVal = (index, e, type, isCheck) => {
+    inputChangeHandler(index, e, type, !isCheck);
+  };
+
+  const conditionCheck = () => {
+    let temp = [...subOptions];
+    let newArr = temp.filter((item) =>
+      ids.find(({ id, type, isCheck }) => isCheck && item.id === id)
+    );
+    console.log(newArr);
+    setNewArr(newArr);
+  };
+
+  useEffect(() => {
+    conditionCheck();
+  }, [ids]);
+
+  function inputChangeHandler(
+    inputIdentifire,
+    enteredId,
+    enteredValue,
+    ischecked
+  ) {
+    let temp = [...ids];
+    temp[inputIdentifire] = {
+      id: enteredId,
+      type: enteredValue,
+      isCheck: ischecked,
+    };
+
+    setIds(temp);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="mainContainer">
+      <div className="header">
+        {ids.map((item, index) => {
+          return (
+            <label
+              htmlFor={`n${item.id}`}
+              className={"button_check"}
+              key={item.id}
+            >
+              <input
+                type={"checkbox"}
+                name={item.type}
+                value={item.id}
+                onChange={() => setVal(index, item.id, item.type, item.isCheck)}
+                id={`n${item.id}`}
+              />
+              {item.type}
+            </label>
+          );
+        })}
+      </div>
+
+      <div className="mainContent">
+        {!newArr.length <= 0
+          ? newArr.map((item, index) => {
+              return (
+                <p key={index} className={"renderSecond"}>
+                  {item.value}
+                </p>
+              );
+            })
+          : subOptions.map((item, index) => {
+              return (
+                <p key={index} className={"renderSecond"}>
+                  {item.value}
+                </p>
+              );
+            })}
+      </div>
+    </section>
   );
 }
 
---
+++ src/data/dummy-data.js
@@ -0,0 +1,53 @@
+export const data = {
+  option: [
+    {
+      id: 30,
+      type: "Mango",
+      isCheck: false,
+    },
+    {
+      id: 20,
+      type: "Apple",
+      isCheck: false,
+    },
+    {
+      id: 40,
+      type: "Orange",
+      isCheck: false,
+    },
+  ],
+  subOption: [
+    {
+      id: 20,
+      value: "yellow",
+    },
+    {
+      id: 20,
+      value: "Red",
+    },
+    {
+      id: 20,
+      value: "Blue",
+    },
+    {
+      id: 40,
+      value: "pink",
+    },
+    {
+      id: 30,
+      value: "white",
+    },
+    {
+      id: 40,
+      value: "offWhite",
+    },
+    {
+      id: 30,
+      value: "grey",
+    },
+    {
+      id: 50,
+      value: "magenda",
+    },
+  ],
+};
